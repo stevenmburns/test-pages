f7dba25e79168d180fde1211b4410d064e64f3a0
Steven Burns <smburns47@gmail.com>
2019-03-05 09:55:45 -0800

    Refactoring

--- hello-world/src/components/Artifacts.vue
+++ hello-world/src/components/Artifacts.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
     <h1>Circle CI Artifacts</h1>
-    <div v-html="badge_svg"></div>
-    <div>{{ badge_url }}</div>
-    <div v-html="badge_img_html"></div>
+    <div v-for="(el,idx) in badge_img_htmls" :key="idx">
+      {{ el.name }}
+      <span v-html="el.img_html"></span>
+    </div>
+    <div>{{ badge_urls[0] }} {{ badge_urls[1] }}</div>
   </div>
 </template>
 
@@ -17,8 +19,9 @@ export default {
   components: {},
   data() {
     return {
-      badge_url: "",
-      badge_svg: ""
+      badge_urls: [],
+      badge_url_tally: "",
+      badge_url_sgi: ""
     };
   },
   methods: {
@@ -29,20 +32,32 @@ export default {
             p.path == "root/project/coverage-reports/tally/htmlcov/coverage.svg"
           );
         });
-        this.badge_url = p.url;
-        var url = "https://cors-anywhere.herokuapp.com/" + p.url;
-        apiService
-          .getURL(url)
-          .then(data => {
-            this.badge_svg = data.data;
-          })
-          .catch(error => console.log(error));
+        this.badge_url_tally = p.url;
+        this.badge_urls.push(p.url);
+      });
+      apiService.getLatestArtifacts().then(artifactJson => {
+        var p = artifactJson.data.find(function(p) {
+          return (
+            p.path == "root/project/coverage-reports/sgi/htmlcov/coverage.svg"
+          );
+        });
+        this.badge_url_sgi = p.url;
+        this.badge_urls.push(p.url);
       });
     }
   },
   computed: {
-    badge_img_html: function() {
-      return "<img src=" + this.badge_url + "></>";
+    badge_img_html_tally: function() {
+      return "<img src=" + this.badge_urls[0] + "></>";
+    },
+    badge_img_html_sgi: function() {
+      return "<img src=" + this.badge_urls[1] + "></>";
+    },
+    badge_img_htmls: function() {
+      return [
+        { name: "tally", img_html: this.badge_img_html_tally },
+        { name: "sgi", img_html: this.badge_img_html_sgi }
+      ];
     }
   },
   mounted() {
